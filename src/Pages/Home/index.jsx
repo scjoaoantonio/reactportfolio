532cef6e58e79b8543646b7bf0190f212a964c7b
João Antônio <joaoasccc@gmail.com>
2023-09-25 22:33:47 -0300

    +about_me
    adicionei um texto sobre mim (risos)

--- src/Pages/Home/index.jsx
+++ src/Pages/Home/index.jsx
@@ -6,13 +6,21 @@ import '../../global.css'
 export const Home = () =>{
     return(
         <section className='home'>
-            <div className="titulo">
-                <div className="container2">
-                <img src={Foto} width="300" className="foto" alt='Joao'/>
+                <div className="container1">
+                    <img src={Foto} width="300" className="foto" alt='Joao'/>
+                    <h1 className="site-title">João Antônio</h1>
+                    <h1 className="site-subtitle">Estudante de Ciência da Computação</h1>
                 </div>
-                <h1 className="site-title">João Antônio</h1>
-                <h1 className="site-subtitle">Estudante de Ciência da Computação & Desenvolvimento Web</h1>
-            </div>
+                <div className='container2'>
+                    <h2>Sobre mim</h2>
+                    <p className='site-apresentation'>
+                        Sou <b>João Antônio</b>, faço <b>Ciência da Computação</b> na Universidade Federal de São João del Rei e estudo <b>Desenvolvimento Web</b>  desde 2020.
+                        <div className='space'>*</div>
+                        Estou em busca de um <b> estágio</b> para desenvolver meu conhecimento, adquirir experiência prática e contribuir para o sucesso da empresa. 
+                        <div className='space'>*</div>
+                        Estou motivado a <b>aprender</b> qualquer tecnologia ou framework necessários para o trabalho.
+                    </p>
+                </div>
         </section>
     );
 };
